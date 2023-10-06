b9605602ab5e5d8953fcbeb88754f39ea63f21c8
Luiz henrique <94985541+LuizHenr1que@users.noreply.github.com>
2023-10-06 12:57:38 -0300

    aprendendo mais de  javavscript

---
+++ Objetos/native,host e user 1/script.js
@@ -0,0 +1,33 @@
+/*console.log(typeof Array.fromsadkksdaak !== "undefined");
+
+if(typeof Array.fromsadkksdaak !== "undefined") {
+    console.log('Existe')
+
+} else {
+    console.log('Não existe')
+}
+*/
+
+
+//liste 5 objetos nativos
+Object 
+String 
+Array
+Number
+function;
+
+//liste 5 objetos browser
+window
+history
+document
+HTMLAllCollection
+NodeList
+
+//liste 2 metodos, propriedades ou objetos
+// presentes no chrome que nao existem no firefox
+if (typeof document.webkitVisibilityState !== "undefined"){
+    console.log('Existe')
+} else {
+    console.log('Não existe')
+};
+
